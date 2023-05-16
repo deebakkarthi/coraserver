529dffc3256cda40804885c669eb69f0f071ba34
Deebakkarthi C R <dbk@deebakkarthi.com>
2023-05-16 08:23:01 +0530

    Add: DB files

---
+++ db/scripts/create.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS classroom (
+    id UUID,
+    name VARCHAR(64) NOT NULL, 
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS slot (
+    id INT,
+    stime TIME NOT NULL, 
+    etime TIME NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS faculty (
+    id CHAR(254),
+    name VARCHAR(64) NOT NULL, 
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS subject (
+    id CHAR(8),
+    name VARCHAR(64) NOT NULL, 
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS static (
+    class_id UUID,
+    day ENUM ("MON", "TUE", "WED", "THU", "FRI"), 
+    slot_id INT, 
+    faculty_id CHAR(254) NOT NULL, 
+    subject_id CHAR(8) NOT NULL, 
+    FOREIGN KEY (class_id) REFERENCES classroom (id), 
+    FOREIGN KEY (slot_id) REFERENCES slot (id), 
+    FOREIGN KEY (faculty_id) REFERENCES faculty (id), 
+    FOREIGN KEY (subject_id) REFERENCES subject (id), 
+    PRIMARY KEY (class_id, day, slot_id)
+);
+CREATE TABLE IF NOT EXISTS dynamic (
+    class_id UUID,
+    date DATE, 
+    slot_id INT, 
+    faculty_id CHAR(254) NOT NULL, 
+    subject_id CHAR(8) NOT NULL, 
+    FOREIGN KEY (class_id) REFERENCES classroom (id), 
+    FOREIGN KEY (faculty_id) REFERENCES faculty (id), 
+    FOREIGN KEY (slot_id) REFERENCES slot (id), 
+    FOREIGN KEY (subject_id) REFERENCES subject (id), 
+    PRIMARY KEY (class_id, date, slot_id)
+);
